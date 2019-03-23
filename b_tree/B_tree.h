1d3e2bae7e23869a09c4c5e6eccf6b2c50567231
Artyom <artemhevorhian@gmail.com>
2019-03-23 12:51:15 +0200

    doing b tree

--- b_tree/B_tree.h
+++ b_tree/B_tree.h
@@ -7,34 +7,42 @@
 
 #define num_limit long long
 
+// use node_key([shared_ptr<Node <T>>]) to get the node's key hash value
+#define get_key(k) k -> m_data_ptr -> getKey()
+
 #include <memory>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+
+template <typename T> class Node;
 
 using std::pair;
 using std::shared_ptr;
 using std::make_shared;
 using std::vector;
 
+template <typename T>
+using shared_node_t = shared_ptr<Node <T>>;
+
 class Book {
 public:
-    //Book() = default;
 
-    explicit Book(const int &key) : m_key(key) {}
+    explicit Book(const unsigned &key) : m_key(key) {}
 
-    int getKey() const {
+    unsigned getKey() const {
         return m_key;
     }
 
 private:
-    int m_key;
+    unsigned m_key;
 };
 
 // Node вызывает само дерево извне оно невызывается запятые потерял
 template <typename T>
 struct Node {
 
-    Node (const T data, int balance_factor)
+    Node (const T data, unsigned balance_factor)
 
     {
         m_is_leaf = true;
@@ -47,7 +55,7 @@ struct Node {
     shared_ptr <T> m_data_ptr;
     vector <shared_ptr<Node<T>>> m_children;
     vector <num_limit> m_keys;
-    int m_children_count;
+    unsigned m_children_count;
     bool m_is_leaf;
 };
 
@@ -55,12 +63,20 @@ template <typename T>
 class B_tree {
 public:
 
-    explicit B_tree(const T root, int balance_factor) : m_counter(0), m_balance_factor(balance_factor)
+    explicit B_tree(const T root, unsigned balance_factor) : m_counter(0), m_balance_factor(balance_factor)
     {
         m_root = make_shared<Node<T>>(root, balance_factor);
     }
 
     //--- access operations
+
+    pair <shared_node_t <T>, unsigned> search(shared_node_t <T> , Node <T>) {
+        unsigned i = 1;
+
+    }
+
+    // overload for integer key!
+
 
     // modification operations
 
@@ -68,8 +84,8 @@ private:
 
     // пр балансе надо смотреть не на m_counter а на кол-во узлов внутри !
     shared_ptr <Node<T>> m_root;
-    int m_counter;
-    int m_balance_factor;
+    unsigned m_counter;
+    unsigned m_balance_factor;
 
     // --- b-tree specific helping operations
 };
